3bf7689a740f9bbf54d5f4dc58a39505f54ab883
Daniele Contessi <contessi.daniele@gmail.com>
2021-10-25 10:37:21 +0200

    First try with develop branch

---
+++ prove.sh
@@ -0,0 +1,1 @@
+# python train.py --model gan_es_1d  --dataset XXZ_200_BKT_new  --training_reg 0 -0.65  --validation_reg -0.65 -0.8 --wandb_proj gan --wandb_account cerbero94  --val_thr 5e-3 --tr_thr 5e-4 --niter 400 --G_lr 1e-3 --D_lr 1e-5 
